07eda98a97edbc6c9f7e9bfe7555690462a0044a
pezhmanazar <dr.pezhman.azar@gmail.com>
2025-12-10 14:29:23 +0330

    auto-save

--- constants/backend.ts
+++ constants/backend.ts
@@ -1,8 +1,11 @@
-// phoenix-app/constants/backend.ts
 const LOCAL = "http://192.168.100.4:4000";
 const PROD = "https://qoqnoos.app";
 
 const BACKEND_URL =
   process.env.EXPO_PUBLIC_BACKEND_URL || (__DEV__ ? LOCAL : PROD);
+
+if (__DEV__) {
+  console.log("[BACKEND] BACKEND_URL =", BACKEND_URL);
+}
 
 export default BACKEND_URL;
