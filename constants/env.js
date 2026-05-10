100450f986431e0db3b18fe966520f021ec63c47
pezhmanazar <dr.pezhman.azar@gmail.com>
2026-05-10 12:12:46 +0330

    auto-save

--- constants/env.js
+++ constants/env.js
@@ -1,8 +1,9 @@
 import BACKEND_URL from "./backend";
 
-const APP_API_URL = (process.env.EXPO_PUBLIC_APP_API_URL || BACKEND_URL).replace(/\/+$/, "");
+export const APP_API_URL = (process.env.EXPO_PUBLIC_APP_API_URL || BACKEND_URL).replace(/\/+$/, "");
 
 export { BACKEND_URL };
+
 export const toApi = (path = "/") => {
   const p = String(path || "/");
   return `${BACKEND_URL}${p.startsWith("/") ? p : `/${p}`}`;
