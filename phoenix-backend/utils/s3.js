2e4f6c0e332c39beb0cf9120d036555fdc8c0fe8
pezhmanazar <dr.pezhman.azar@gmail.com>
2026-05-25 13:23:42 +0330

    auto-save

--- phoenix-backend/utils/s3.js
+++ phoenix-backend/utils/s3.js
@@ -38,16 +38,6 @@ export async function uploadBufferToS3({ key, buffer, contentType }) {
     throw new Error("uploadBufferToS3: key and buffer are required");
   }
 
-  // DEBUG (temporary)
-  console.log("[PANAH_S3 upload]", {
-    endpoint: PANAH_S3_ENDPOINT,
-    region: PANAH_S3_REGION,
-    bucket: PANAH_S3_BUCKET,
-    forcePathStyle: true,
-    key,
-    contentType,
-  });
-
   const command = new PutObjectCommand({
     Bucket: PANAH_S3_BUCKET,
     Key: key,
